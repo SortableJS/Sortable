48aa1fc3b41d434e114ee671ca528ece2491ee0b
RubaXa <ibnRubaXa@gmail.com>
2014-11-27 23:15:44 +0300

    + jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,40 @@
-'use strict';
+module.exports = function (grunt) {
+	'use strict';
 
-module.exports = function (grunt){
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
 		version: {
 			src: ['<%= pkg.exportName %>.js', '*.json']
+		},
+
+		jshint: {
+			all: ['*.js', '!*.min.js'],
+
+			options: {
+				strict: true,
+				newcap: false,	// "Tolerate uncapitalized constructors"
+				node: true,
+				expr: true, // - true && call() "Expected an assignment or function call and instead saw an expression."
+				supernew: true, // - "Missing '()' invoking a constructor."
+				laxbreak: true,
+				white: true,
+				globals: {
+					define: true,
+					test: true,
+					expect: true,
+					module: true,
+					asyncTest: true,
+					start: true,
+					ok: true,
+					equal: true,
+					notEqual: true,
+					deepEqual: true,
+					window: true,
+					document: true,
+					performance: true
+				}
+			}
 		},
 
 		uglify: {
@@ -14,18 +43,18 @@ module.exports = function (grunt){
 			},
 			dist: {
 				files: {
-					  '<%= pkg.exportName %>.min.js': ['<%= pkg.exportName %>.js']
+					'<%= pkg.exportName %>.min.js': ['<%= pkg.exportName %>.js']
 				}
 			}
 		}
 	});
 
 
-	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-version');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 
-	// Default task.
-	grunt.registerTask('default', ['version', 'uglify']);
+	grunt.registerTask('tests', ['jshint']);
+	grunt.registerTask('default', ['tests', 'version', 'uglify']);
 };
