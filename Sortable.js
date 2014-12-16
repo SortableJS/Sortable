6656e45f302469f37aa7da0f9b68d70ff0b2f100
RubaXa <ibnRubaXa@gmail.com>
2014-12-16 20:50:38 +0300

    #166: fixed _onDragOver & group

--- Sortable.js
+++ Sortable.js
@@ -105,7 +105,8 @@
 				dataTransfer.setData('Text', dragEl.textContent);
 			}
 		},
-		group;
+
+		group = options.group;
 
 
 		// Set default options
@@ -114,10 +115,9 @@
 		}
 
 
-		if (!options.group.name) {
-			options.group = { name: options.group };
+		if (!group || typeof group != 'object') {
+			group = options.group = { name: group };
 		}
-		group = options.group;
 
 
 		['pull', 'put'].forEach(function (key) {
@@ -450,7 +450,10 @@
 				isOwner = (activeGroup === group),
 				canSort = options.sort;
 
-			(evt.stopPropagation !== void 0) && evt.stopPropagation();
+			if (evt.preventDefault !== void 0) {
+				evt.preventDefault();
+				evt.stopPropagation();
+			}
 
 			if (!_silent && activeGroup &&
 				(isOwner
