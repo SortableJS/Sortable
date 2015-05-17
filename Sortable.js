45e5a9535bed7c5a0439f83de9134958858e11ae
Brad Wertman <brad.wertman@leanmeanz.com>
2015-05-17 09:39:03 -0400

    Adds ability to hide the ghost and child elements with ghostClass
    I  was adding `display: none;` to a child button of the draggable element. The button element itself would hide onDragStart but it's ghost would not. This fixes that and makes it so that the entire ghost or any of it's child elements can be hidden with the ghostClass

--- Sortable.js
+++ Sortable.js
@@ -486,6 +486,7 @@
 				_css(ghostEl, 'opacity', '0.8');
 				_css(ghostEl, 'position', 'fixed');
 				_css(ghostEl, 'zIndex', '100000');
+				_css(ghostEl, 'display', 'none');
 
 				rootEl.appendChild(ghostEl);
 
