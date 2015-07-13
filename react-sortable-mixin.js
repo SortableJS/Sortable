44f90470b6ff19f166e89e0c7db328844c1ee344
Jared Chapiewsky <jpchapiewsky@gmail.com>
2015-07-13 16:35:19 -0500

    Added try catch block around insertBefore
    Fix for code throwing "Error: Failed to execute 'insertBefore' on 'Node': The node before which the new node is to be inserted is not a child of this node."

--- react-sortable-mixin.js
+++ react-sortable-mixin.js
@@ -98,7 +98,11 @@
 						_activeComponent = this;
 					}
 					else if (name === 'onAdd' || name === 'onUpdate') {
-						evt.from.insertBefore(evt.item, _nextSibling);
+						try {
+							evt.from.insertBefore(evt.item, _nextSibling);
+						}catch(ex){
+							evt.from.appendChild(evt.item);
+						}
 
 						var newState = {},
 							remoteState = {},
