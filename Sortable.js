86a6ba7088da0088edce89ab80af390781ac994c
alexk111 <alexkaul@googlemail.com>
2016-06-04 12:32:16 +0700

    Fixed Destroy workflow

--- Sortable.js
+++ Sortable.js
@@ -23,7 +23,7 @@
 	}
 })(function () {
 	"use strict";
-	
+
 	if (typeof window == "undefined" || typeof window.document == "undefined") {
 		return function() {
 			throw new Error( "Sortable.js requires a window with a document" );
@@ -813,9 +813,8 @@
 						this.save();
 					}
 				}
-
+				this._nulling();
 			}
-			this._nulling();
 		},
 
 		_nulling: function() {
