946bec37c54c49d2508787b7b80455cf7150cfd3
Lebedev Konstantin <ibnRubaXa@gmail.com>
2017-02-19 11:04:02 +0300

    #973: * touchmove

--- Sortable.js
+++ Sortable.js
@@ -1428,6 +1428,12 @@
 		}
 	}
 
+	_on(document, 'touchmove', function (evt) {
+		if (Sortable.active) {
+			evt.preventDefault();
+		}
+	});
+
 	try {
 		window.addEventListener('test', null, Object.defineProperty({}, 'passive', {
 			get: function () {
