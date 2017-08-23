394367a380e9454d48c30e03be6c8f8458150bdc
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2017-08-23 18:57:13 +0530

    Fix issue with evt.path in Firefox, IE

--- Sortable.js
+++ Sortable.js
@@ -310,7 +310,7 @@
 				type = evt.type,
 				touch = evt.touches && evt.touches[0],
 				target = (touch || evt).target,
-				originalTarget = evt.target.shadowRoot && evt.path[0] || target,
+				originalTarget = evt.target.shadowRoot && (evt.path && evt.path[0]) || target,
 				filter = options.filter,
 				startIndex;
 
