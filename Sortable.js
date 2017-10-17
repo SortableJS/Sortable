5cf06ee535ad3edecee4997dad0475742c334892
Aline Lee Nogueira and William Seiti Mizuta <login+alinelee+williammizuta@elo7.com>
2017-10-17 18:36:01 -0200

    Elo7 async define friendly

--- Sortable.js
+++ Sortable.js
@@ -6,17 +6,7 @@
 
 (function sortableModule(factory) {
 	"use strict";
-
-	if (typeof define === "function" && define.amd) {
-		define(factory);
-	}
-	else if (typeof module != "undefined" && typeof module.exports != "undefined") {
-		module.exports = factory();
-	}
-	else {
-		/* jshint sub:true */
-		window["Sortable"] = factory();
-	}
+	define('sortable', [], factory);
 })(function sortableFactory() {
 	"use strict";
 
