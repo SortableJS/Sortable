47de7844aa88556133384211620ada8a7dea3f65
RubaXa <k.lebedev@corp.mail.ru>
2017-10-28 10:19:45 +0300

    * rebuild

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,5 +98,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-testcafe');
 
 	grunt.registerTask('tests', ['jshint']);
-	grunt.registerTask('default', ['tests', 'version', 'uglify:dist', 'testcafe']);
+	grunt.registerTask('build', ['version', 'uglify:dist']);
+	grunt.registerTask('default', ['tests', 'build', 'testcafe']);
 };
