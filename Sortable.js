SortableJS/Sortable

commit b8a358a5f653e35c5d14837d886673c8584d2018

Use Element.matches for improved selector support

Sortable.js
@@ -1427,15 +1427,11 @@
 
 	function _matches(/**HTMLElement*/el, /**String*/selector) {
 		if (el) {
-			selector = selector.split('.');
-
-			var tag = selector.shift().toUpperCase(),
-				re = new RegExp('\\s(' + selector.join('|') + ')(?=\\s)', 'g');
-
-			return (
-				(tag === '' || el.nodeName.toUpperCase() == tag) &&
-				(!selector.length || ((' ' + el.className + ' ').match(re) || []).length == selector.length)
-			);
+			if (el.matches) {
+				return el.matches(selector);
+			} else if (el.msMatchesSelector) {
+				return el.msMatchesSelector(selector);
+			}
 		}
 
 		return false;

commit 3785502f1f4fece876ff40a64a973185a5530308

Fix HTMLElement.match error introduced in b8a358a

Sortable.js
@@ -1432,9 +1432,17 @@
 	function _matches(/**HTMLElement*/el, /**String*/selector) {
 		if (el) {
 			if (el.matches) {
-				return el.matches(selector);
+				try {
+					return el.matches(selector);
+				} catch(e) {
+					return false;
+				}
 			} else if (el.msMatchesSelector) {
-				return el.msMatchesSelector(selector);
+				try {
+					return el.msMatchesSelector(selector);
+				} catch(e) {
+					return false;
+				}
 			}
 		}
 