3628ca134c57b5adfab424b4303bdea564c9bf1e
owen-m1 <owen23355@gmail.com>
2019-03-08 19:04:45 -0500

    improved nested sortables demo

--- st/app.js
+++ st/app.js
@@ -194,9 +194,12 @@ new Sortable(gridDemo, {
 // Nested demo
 var nestedSortables = [].slice.call(document.querySelectorAll('.nested-sortable'));
 
+// Loop through each nested sortable element
 for (var i = 0; i < nestedSortables.length; i++) {
 	new Sortable(nestedSortables[i], {
 		group: 'nested',
-		animation: 150
+		animation: 150,
+		fallbackOnBody: true,
+		swapThreshold: 0.65
 	});
 }
