3a3f4e14d3a61bdf5eced8694cd4d6d17387b0f2
owen-m1 <owen23355@gmail.com>
2019-06-05 20:38:42 -0400

    Updated demo

--- st/app.js
+++ st/app.js
@@ -8,7 +8,9 @@ var example1 = document.getElementById('example1'),
 	example5 = document.getElementById('example5'),
 	example6 = document.getElementById('example6'),
 	example7 = document.getElementById('example7'),
-	gridDemo = document.getElementById('gridDemo');
+	gridDemo = document.getElementById('gridDemo'),
+	multiDragDemo = document.getElementById('multiDragDemo'),
+	swapDemo = document.getElementById('swapDemo');
 
 // Example 1 - Simple list
 new Sortable(example1, {
@@ -194,9 +196,27 @@ new Sortable(gridDemo, {
 // Nested demo
 var nestedSortables = [].slice.call(document.querySelectorAll('.nested-sortable'));
 
+// Loop through each nested sortable element
 for (var i = 0; i < nestedSortables.length; i++) {
 	new Sortable(nestedSortables[i], {
 		group: 'nested',
-		animation: 150
+		animation: 150,
+		fallbackOnBody: true,
+		swapThreshold: 0.65
 	});
 }
+
+// MultiDrag demo
+new Sortable(multiDragDemo, {
+	multiDrag: true,
+	selectedClass: 'selected',
+	animation: 150
+});
+
+
+// Swap demo
+new Sortable(swapDemo, {
+	swap: true,
+	swapClass: 'highlight',
+	animation: 150
+});
