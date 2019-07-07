9348405ecb9d74457c4d7f9ddba77aa0c6a37d1f
owen-m1 <owen23355@gmail.com>
2019-07-07 16:55:21 -0400

    back to circleci

--- scripts/test-compat.js
+++ scripts/test-compat.js
@@ -2,10 +2,10 @@ const createTestCafe = require('testcafe');
 // Testcafe cannot test on IE < 11
 // Testcafe testing on Chrome Android is currently broken (https://github.com/DevExpress/testcafe/issues/3948)
 const browsers = [
-	'ie',
+	'browserstack:ie@11.0:Windows 10',
 	'chrome:headless',
 	'firefox:headless',
-	'edge',
+	'browserstack:edge@16.0:Windows 10',
 	'browserstack:safari@12.1:OS X Mojave',
 	'browserstack:iPhone 8@12.1'
 ];
