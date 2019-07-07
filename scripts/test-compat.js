22cbe0f183d46fc4d2811d6701e30e4dbb21ef24
owen-m1 <owen23355@gmail.com>
2019-07-07 09:27:41 -0400

    fixing tests

--- scripts/test-compat.js
+++ scripts/test-compat.js
@@ -2,10 +2,10 @@ const createTestCafe = require('testcafe');
 // Testcafe cannot test on IE < 11
 // Testcafe testing on Chrome Android is currently broken (https://github.com/DevExpress/testcafe/issues/3948)
 const browsers = [
-	'ie:headless',
+	'browserstack:ie@11.0:Windows 10',
 	'chrome:headless',
 	'firefox:headless',
-	'edge:headless',
+	'browserstack:edge@16.0:Windows 10',
 	'browserstack:safari@12.1:OS X Mojave',
 	'browserstack:iPhone 8@12.1'
 ];
@@ -13,7 +13,7 @@ let testcafe;
 let runner;
 
 
-createTestCafe().then((tc) => {
+createTestCafe(null, 8000, 8001).then((tc) => {
 	testcafe = tc;
 	runner = tc.createRunner();
 	return runner
