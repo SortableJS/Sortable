b38b21802a6fe0f959aefc6a0ada75a74c86de5a
owen-m1 <owen23355@gmail.com>
2019-07-09 09:03:05 -0400

    try saucelabs

--- scripts/test-compat.js
+++ scripts/test-compat.js
@@ -2,12 +2,16 @@ const createTestCafe = require('testcafe');
 // Testcafe cannot test on IE < 11
 // Testcafe testing on Chrome Android is currently broken (https://github.com/DevExpress/testcafe/issues/3948)
 const browsers = [
-	'browserstack:ie@11.0:Windows 10',
-	'chrome:headless',
-	'firefox:headless',
-	'browserstack:edge@16.0:Windows 10',
-	'browserstack:safari@12.1:OS X Mojave',
-	'browserstack:iPhone 8@12.1'
+	'saucelabs:iPhone XS Simulator@12.2',
+	'saucelabs:Internet Explorer@9.0:Windows 7'
+	//
+	// 'saucelabs:Chrome@75.0:Windows 10'
+	//
+	// 'chrome:headless',
+	// 'firefox:headless',
+	// 'browserstack:edge@16.0:Windows 10',
+	// 'browserstack:safari@12.1:OS X Mojave',
+	// 'browserstack:iPhone 8@12.1'
 ];
 let testcafe;
 let runner;
