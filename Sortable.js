ed81feb9de83351f61fd932616935bca31bccf32
Wayne Van Son <waynevspersonal@gmail.com>
2020-01-10 13:14:38 +1100

    rollup adds  bracket  an anon function

--- Sortable.js
+++ Sortable.js
@@ -8,7 +8,7 @@
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	(global = global || self, global.Sortable = factory());
-}(this, function () { 'use strict';
+}(this, (function () { 'use strict';
 
 	var version = "1.10.2";
 
@@ -3730,4 +3730,4 @@
 
 	return Sortable;
 
-}));
+})));
