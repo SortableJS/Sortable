fa03814c355c70fba659d2027dc3bd9eb3778390
Wayne Van Son <waynevspersonal@gmail.com>
2020-01-11 15:54:35 +1100

    add better promise handling.

--- scripts/test.js
+++ scripts/test.js
@@ -1,21 +1,23 @@
-const createTestCafe = require('testcafe');
+const createTestCafe = require("testcafe");
 
 let testcafe;
 let runner;
-let failedCount;
+let errorCode;
 
-
-createTestCafe().then((tc) => {
-	testcafe = tc;
-	runner = tc.createRunner();
-	return runner
-		.src('./tests/Sortable.test.js')
-		.browsers('chrome:headless')
-		.concurrency(3)
-		.run();
-}).then((actualFailedCount) => {
-	failedCount = actualFailedCount;
-	console.log('FAILED COUNT', actualFailedCount)
-    return testcafe.close();
-}).then(() => process.exit(failedCount));
-
+createTestCafe()
+	.then(tc => {
+		testcafe = tc;
+		runner = tc.createRunner();
+		return runner
+			.src("./tests/Sortable.test.js")
+			.browsers("chrome:headless")
+			.concurrency(3)
+			.run();
+	})
+	.then(actualFailedCount => {
+		errorCode = actualFailedCount > 0 ? 1 : 0;
+		console.log("FAILED COUNT", actualFailedCount);
+		return testcafe.close();
+	})
+	.catch(err => console.error(err))
+	.finally(() => process.exit(errorCode));
