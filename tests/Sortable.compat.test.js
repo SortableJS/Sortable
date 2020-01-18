3582c7de382fec868cfb15f47cee725744927cd9
Wayne Van Son <waynevspersonal@gmail.com>
2020-01-18 14:22:43 +1100

    add try/catch to browser tests

--- tests/Sortable.compat.test.js
+++ tests/Sortable.compat.test.js
@@ -1,12 +1,10 @@
-import { Selector } from 'testcafe';
+import { Selector } from "testcafe";
 
+fixture`Simple Sorting`.page`./single-list.html`;
 
-fixture `Simple Sorting`
-	.page `./single-list.html`;
+let list1 = Selector("#list1");
 
-let list1 = Selector('#list1');
-
-test('Sort down list', async browser => {
+test("Sort down list", async browser => {
 	const dragStartPosition = list1.child(0);
 	const dragEl = await dragStartPosition();
 	const dragEndPosition = list1.child(2);
@@ -14,26 +12,41 @@ test('Sort down list', async browser => {
 	const target = await targetStartPosition();
 	const targetEndPosition = list1.child(1);
 
-	await browser
-		.expect(dragStartPosition.innerText).eql(dragEl.innerText)
-		.expect(targetStartPosition.innerText).eql(target.innerText)
-		.dragToElement(dragEl, target)
-		.expect(dragEndPosition.innerText).eql(dragEl.innerText)
-		.expect(targetEndPosition.innerText).eql(target.innerText);
+	try {
+		await browser
+			.expect(dragStartPosition.innerText)
+			.eql(dragEl.innerText)
+			.expect(targetStartPosition.innerText)
+			.eql(target.innerText)
+			.dragToElement(dragEl, target)
+			.expect(dragEndPosition.innerText)
+			.eql(dragEl.innerText)
+			.expect(targetEndPosition.innerText)
+			.eql(target.innerText);
+	} catch (error) {
+		throw new Error(error);
+	}
 });
 
-test('Sort up list', async browser => {
+test("Sort up list", async browser => {
 	const dragStartPosition = list1.child(2);
 	const dragEl = await dragStartPosition();
 	const dragEndPosition = list1.child(0);
 	const targetStartPosition = list1.child(0);
 	const target = await targetStartPosition();
 	const targetEndPosition = list1.child(1);
-
-	await browser
-		.expect(dragStartPosition.innerText).eql(dragEl.innerText)
-		.expect(targetStartPosition.innerText).eql(target.innerText)
-		.dragToElement(dragEl, target)
-		.expect(dragEndPosition.innerText).eql(dragEl.innerText)
-		.expect(targetEndPosition.innerText).eql(target.innerText);
+	try {
+		await browser
+			.expect(dragStartPosition.innerText)
+			.eql(dragEl.innerText)
+			.expect(targetStartPosition.innerText)
+			.eql(target.innerText)
+			.dragToElement(dragEl, target)
+			.expect(dragEndPosition.innerText)
+			.eql(dragEl.innerText)
+			.expect(targetEndPosition.innerText)
+			.eql(target.innerText);
+	} catch (error) {
+		throw new Error(error);
+	}
 });
