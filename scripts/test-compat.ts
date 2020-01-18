d152e1defeac436bd11ed6db089ce377f5c33b7e
Wayne Van Son <waynevspersonal@gmail.com>
2020-01-18 16:24:36 +1100

    use list plugin for testcafe

--- scripts/test-compat.ts
+++ scripts/test-compat.ts
@@ -23,7 +23,7 @@ async function testCompat() {
 		.browsers(browsers)
 		// This error is annoying but required so we can see the errors in `stdout`.
 		// — "The "reporter" option from the configuration file will be ignored.
-		.reporter("spec", process.stdout);
+		.reporter("list", process.stdout);
 
 	console.log(`Test cafe runner created. Running tests from "${dir}"...`);
 
