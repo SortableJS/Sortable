77112076111a2521244dba3e3eb9487a7f4b5c1e
Wayne Van Son <waynevanson@gmail.com>
2020-09-21 03:24:36 +0000

    fix(build): corrects exports for compatibility (#1926)
    * refactor: remove code formattings config files
    
    * docs: add more detail to contribution guidelines
    
    * docs: remove custom templates
    
    * docs: update bug report issue
    
    * docs: add discussion template
    
    * docs: update featue template
    
    * fix(build): build dist/sortablejs, was missing prior
    
    * build(deps): yarn upgrade
    
    * v1.11.2-alpha.4
    
    * fix(build): exports modules correctly
    
    This was broken, visible, and closes #1891.

--- packages/sortablejs/src/default-umd.ts
+++
@@ -1,9 +0,0 @@
-import Sortable from "../../sortable/src/Sortable";
-import AutoScroll from "../../plugins/auto-scroll/src";
-import { RemoveOnSpill, RevertOnSpill } from "../../plugins/on-spill/src";
-
-//@ts-ignore
-Sortable.mount(new AutoScroll());
-Sortable.mount(RemoveOnSpill, RevertOnSpill);
-
-export default Sortable;
