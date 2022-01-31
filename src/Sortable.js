c72bb414fe9258c5e6b8e452b7fc09dc18980cd2
Laineus <lainen000@gmail.com>
2022-01-31 13:24:09 +0900

    styling by direction

--- src/Sortable.js
+++ src/Sortable.js
@@ -817,8 +817,13 @@ Sortable.prototype = /** @lends Sortable.prototype */ {
 
 			if (ghostEl) {
 				if (ghostMatrix) {
-					ghostMatrix.e += dx - (lastDx || 0);
-					ghostMatrix.f += dy - (lastDy || 0);
+					const vertical = this._getDirection(evt, target) === 'vertical';
+					if (!vertical) {
+						ghostMatrix.e += dx - (lastDx || 0);
+					}
+					if (vertical) {
+						ghostMatrix.f += dy - (lastDy || 0);
+					}
 				} else {
 					ghostMatrix = {
 						a: 1,
